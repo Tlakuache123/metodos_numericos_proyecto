cd6f71efa2a20eaf7cec56e27467511ec76ae0a7
srTlakuache <calloman123@gmail.com>
2022-10-05 22:00:01 -0500

    Early Menu

--- main.cpp
+++ main.cpp
@@ -3,8 +3,22 @@
 
 using namespace std;
 
+void print_metodos(){
+    cout << "1- Falsa Posicion" << endl;
+    return;
+}
+
 int main (int argc, char *argv[])
 {
-    falsa_posicion();
+    int option = 0;
+
+    print_metodos();
+    cout << "[!] Ingresa el metodo que quieres usar" << endl;
+    cin >> option;
+
+    switch (option) {
+        case 0:
+            falsa_posicion();
+    }
     return 0;
 }
