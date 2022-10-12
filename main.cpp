78036370d430d1fa637bf9c29db239a99b4c0669
srTlakuache <calloman123@gmail.com>
2022-10-12 03:32:57 -0500

    Preparing for Matrix

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 void print_metodos(){
     cout << "1- Falsa Posicion" << endl;
+    cout << "2- Newton" << endl;
     return;
 }
 
@@ -17,6 +18,7 @@ int main (int argc, char *argv[])
     cout << "[!] Ingresa el metodo que quieres usar" << endl;
     cin >> option;
 
+    option -= 1;
     switch (option) {
         case 0:
             falsa_posicion();
