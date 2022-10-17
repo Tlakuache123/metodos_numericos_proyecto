fc2526478d00a774e472f27afc9ae98a8c4cafc6
srTlakuache <calloman123@gmail.com>
2022-10-17 18:26:13 -0500

    Preparing to merge with matrix

--- main.cpp
+++ main.cpp
@@ -12,18 +12,5 @@ void print_metodos(){
 
 int main (int argc, char *argv[])
 {
-    int option = 0;
-
-    print_metodos();
-    cout << "[!] Ingresa el metodo que quieres usar" << endl;
-    cin >> option;
-
-    option -= 1;
-    switch (option) {
-        case 0:
-            falsa_posicion();
-        case 1:
-            newton();
-    }
     return 0;
 }
