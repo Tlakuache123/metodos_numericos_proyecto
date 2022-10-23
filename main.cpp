0325cb447996a6f66062fe395f66f87af54f3e0b
Claudio Tlakuache <claussielTlakuache@gmail.com>
2022-10-22 21:23:40 -0500

    Preparing to merge

--- main.cpp
+++ main.cpp
@@ -15,23 +15,5 @@ void print_metodos(){
 
 int main (int argc, char *argv[])
 {
-    vector<vector<double>> m = {
-        {4, 1, 1, 2, 3},
-        {4, 2, 3, 1, 9},
-        {5, 3, 3, -6, 2},
-        {5, 4, -4, 7, 8},
-        {1, 5, 2, 3, 4}
-    };
-
-    vector<vector<double>> b = {
-        {1},
-        {3},
-        {5},
-        {8},
-        {8}
-    };
-    SystemMatrix sys_m(m, b );
-
-    sys_m.gauss_partitions();
     return 0;
 }
